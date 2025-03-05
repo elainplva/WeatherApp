78f00fa3ef704db786b6ed037fc168d03a5f458a
Elain Polakova <S00250500@atu.ie>
2025-03-05 12:23:45 +0000

    getting a date time package

--- WeatherApp/WeatherPage.xaml.cs
+++ WeatherApp/WeatherPage.xaml.cs
@@ -4,14 +4,22 @@ namespace WeatherApp;
 
 public partial class WeatherPage : ContentPage
 {
+	public List<Models.List> WeatherList;
 	public WeatherPage()
 	{
 		InitializeComponent();
+		WeatherList = new List<Models.List>();
 	}
 	protected async override void OnAppearing()
 	{
 		base.OnAppearing();
 		var result = await ApiService.GetWeather(54.27850, 8.45972);
+		foreach (var item in result.list) 
+		{
+              WeatherList.Add(item);
+		}
+		cvWeather.ItemsSource = WeatherList;
+
 		lbCity.Text = result.city.name;
 		lbWeatherDesc.Text = result.list[0].weather[0].description;
 		lbTemperature.Text = result.list[0].main.temperature + "°C";
@@ -19,4 +27,6 @@ public partial class WeatherPage : ContentPage
 		lbWind.Text = result.list[0].wind.speed + "km/h";
 		imgWeatherIcon.Source = result.list[0].weather[0].customIcon;
     }
+
+
 }
