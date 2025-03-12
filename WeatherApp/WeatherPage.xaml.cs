11dbf5fe8814d0c44b4b08310ca3ca50fc8de61e
Elain Polakova <S00250500@atu.ie>
2025-03-12 15:37:58 +0000

    edits to xaml.cs

--- WeatherApp/WeatherPage.xaml.cs
+++ WeatherApp/WeatherPage.xaml.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using WeatherApp.Services;
 
 namespace WeatherApp;
@@ -16,27 +17,60 @@ public partial class WeatherPage : ContentPage
 	{
 		base.OnAppearing();
 		await GetLocation();
-		var result = await ApiService.GetWeather(latitude,longitude);
-		foreach (var item in result.list) 
-		{
-              WeatherList.Add(item);
-		}
-		cvWeather.ItemsSource = WeatherList;
+        await GetWeatherDataByLocation(latitude, longitude);
 
-		lbCity.Text = result.city.name;
-		lbWeatherDesc.Text = result.list[0].weather[0].description;
-		lbTemperature.Text = result.list[0].main.temperature + "°C";
-		lbHumidity.Text = result.list[0].main.humidity + "%";
-		lbWind.Text = result.list[0].wind.speed + "km/h";
-		imgWeatherIcon.Source = result.list[0].weather[0].customIcon;
     }
 
-	public async Task GetLocation()
+    public async Task GetLocation()
 	{
 		var location = await Geolocation.GetLocationAsync();
 		latitude = location.Latitude;
 		longitude = location.Longitude;
 	}
 
+	private async Task TapLocation_Tapped(object sender, EventArgs e)
+	{
+		await GetLocation();
+		await GetWeatherDataByLocation(latitude, longitude);
 
+	}
+
+	public async Task GetWeatherDataByLocation(double latiude, double longitude)
+	{
+        var result = await ApiService.GetWeather(latitude, longitude);
+		UpdateUI(result);
+    }
+
+	private async void ImgButton_Clicked(object sender, EventArgs e)
+	{
+		var response = await DisplayPromptAsync(title: "", message: "", placeholder: "Search weather by city", accept: "Search", cancel: "Cancel");
+		if (response != null)
+		{
+            await GetWeatherDataByCity(response);
+		}
+	}
+
+    public async Task GetWeatherDataByCity(string city)
+    {
+        var result = await ApiService.GetWeatherByCity(city);
+		UpdateUI(result);
+    }
+
+    public void UpdateUI(dynamic result)
+    {
+        foreach (var item in result.list)
+        {
+            WeatherList.Add(item);
+        }
+        cvWeather.ItemsSource = WeatherList;
+
+        lbCity.Text = result.city.name;
+        lbWeatherDesc.Text = result.list[0].weather[0].description;
+        lbTemperature.Text = result.list[0].main.temperature + "°C";
+        lbHumidity.Text = result.list[0].main.humidity + "%";
+        lbWind.Text = result.list[0].wind.speed + "km/h";
+        imgWeatherIcon.Source = result.list[0].weather[0].customIcon;
+
+    }
 }
+
