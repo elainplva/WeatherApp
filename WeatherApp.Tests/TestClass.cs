d69f9719319b28f388b51becb7643756658ed969
Elain Polakova <S00250500@atu.ie>
2025-03-18 10:41:57 +0000

    setting up tests for the project

---
+++ WeatherApp.Tests/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Serialization;
+using NUnit.Framework;
+using WeatherApp;
+
+
+namespace WeatherApp.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+
+        public void Test1()
+        {
+              
+
+        }
+
+    }
+}
