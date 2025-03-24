3e4f53c4f4974c0a482fb14bc6b455cb6b7c4ad8
Elain Polakova <S00250500@atu.ie>
2025-03-24 11:21:49 +0000

    Uploading the Models and Services solution folder. Uploading Roots.cs and ApiService.cs. Making WeatherPage.xaml and WelcomePage.xaml

--- WeatherApp/Models/Root.cs
+++ WeatherApp/Models/Root.cs
@@ -3,11 +3,96 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 
 namespace WeatherApp.Models
 {
-    class Root
+    public class City
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public Coord? Coord { get; set; }
+        public string? Country { get; set; }
+        public int Population { get; set; }
+        public int Timezone { get; set; }
+        public int Sunrise { get; set; }
+        public int Sunset { get; set; }
     }
+
+    public class Clouds
+    {
+        public int All { get; set; }
+    }
+
+    public class Coord
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+    }
+
+    public class List
+    {
+        public int Dt { get; set; }
+        public string DateTime => UtcTimeLibrary.UtcTimeStamp.ConvertToUtc(Dt);
+
+        public Main? Main { get; set; }
+        public List<Weather>? Weather { get; set; }
+        public Clouds? Clouds { get; set; }
+        public Wind? Wind { get; set; }
+        public Sys? Sys { get; set; }
+        public string? Dt_txt { get; set; }
+        public Snow? Snow { get; set; }
+    }
+
+    public class Main
+    {
+        public double Temp { get; set; }
+        public double Temperature => Math.Round(Temp);
+        public double Feels_like { get; set; }
+        public double Temp_min { get; set; }
+        public double Temp_max { get; set; }
+        public int Pressure { get; set; }
+        public int Sea_level { get; set; }
+        public int Grnd_level { get; set; }
+        public int Humidity { get; set; }
+        public double Temp_kf { get; set; }
+    }
+
+    public class Root
+    {
+        public string? Cod { get; set; }
+        public int Message { get; set; }
+        public int Cnt { get; set; }
+        public List<List>? List { get; set; }
+        public City? City { get; set; }
+    }
+    public class Snow
+    {
+        [JsonProperty("3h")]
+        public double _3h { get; set; }
+    }
+
+    public class Sys
+    {
+        public string? Pod { get; set; }
+    }
+
+    public class Weather
+    {
+        public int ID { get; set; }
+        public string? Main { get; set; }
+        public string? Description { get; set; }
+        public string? Icon { get; set; }
+        //public string fullIconUrl => string.Format("https://openweathermap.org/img/wn/{0}@2x.png", icon);
+        public string CustomIcon => string.Format("icon_{0}.png", Icon);
+    }
+
+    public class Wind
+    {
+        public double Speed { get; set; }
+        public int Deg { get; set; }
+        public double Gust { get; set; }
+    }
+
 }
--- WeatherApp/Services/ApiService.cs
+++ WeatherApp/Services/ApiService.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WeatherApp.Models;
 
 namespace WeatherApp.Services
 {
     class ApiService
     {
+        public static async Task<Root> GetWeather(double latitude, double longitude)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(
+                $"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&appid=faeb14dd3f0d880980cf2137b29720e6");
+
+            return JsonConvert.DeserializeObject<Root>(response) ?? throw new Exception("Failed to deserialize weather data.");
+        }
+
+        public static async Task<Root> GetWeatherByCity(string city)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(
+                $"https://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid=faeb14dd3f0d880980cf2137b29720e6");
+
+            return JsonConvert.DeserializeObject<Root>(response) ?? throw new Exception("Failed to deserialize weather data.");
+        }
     }
 }
